007499d2ae9f5f50dd1599fc044ec90c5eccfbd3
Zanini <everton.zanini@yahoo.com>
2022-01-26 16:38:27 -0300

    Projeto finalizado com AXIOS e o ACTIVITYINDICATOR

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { 
-  StyleSheet, 
-  Text, 
+import {
+  StyleSheet,
+  Text,
   View,
-  FlatList
- } from 'react-native';
+  FlatList,
+  ActivityIndicator
+} from 'react-native';
 
 import Filmes from './src/Filmes';
 
@@ -15,25 +16,40 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      filmes:[]
+      loading: true,
+      filmes: []
     }
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     const response = await api.get("r-api/?api=filmes");
-    this.setState({filmes: response.data});
+    this.setState({ filmes: response.data, loading:false });
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.filmes}
-          keyExtractor={item=>item.id}
-          renderItem={({item})=><Filmes/>}
-        />
-      </View>
-    );
+
+    if (this.state.loading) {
+      return(
+        <View style={{flex:1, justifyContent:'center', alignContent:'center'}}>
+          <ActivityIndicator
+            color={'#09a6ff'}
+            size={60}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.filmes}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({ item }) => <Filmes data={item} />}
+          />
+        </View>
+      );
+    }
+
+
   }
 }
 
--- src/Filmes/index.js
+++ src/Filmes/index.js
@@ -1,18 +1,66 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 
-export default class Filmes extends Component{
 
-    constructor(props){
-        super(props);
-        this.state = {
+export default class Filmes extends Component {
+    render() {
+        const { nome, foto, sinopse } = this.props.data;
 
-        };
-    }
-
-    render(){
-        return(
-            <Text>...</Text>
+        return (
+            <View>
+                <View style={styles.card}>
+                    <Text style={styles.titulo}>{nome}</Text>
+                    <Image
+                        style={styles.capa}
+                        source={{ uri: foto }}
+                    />
+                    <View style={styles.areaBotao}>
+                        <TouchableOpacity style={styles.botao} onPress={() => { 
+                            alert(sinopse)
+                        }}>
+                            <Text style={styles.textoBotao}>LEIA MAIS</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    card: {
+        shadowColor: '#000',
+        backgroundColor: '#fff',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        margin: 15,
+        shadowRadius: 5,
+        borderRadius: 5,
+        elevation: 3,
+    },
+    titulo: {
+        fontSize: 18,
+        padding: 15,
+    },
+    capa: {
+        height: 250,
+        zIndex: 2,
+    },
+    areaBotao: {
+        alignItems: 'flex-end',
+        marginTop: -40,
+        zIndex: 9
+    },
+    botao: {
+        width: 100,
+        backgroundColor: "#09a6ff",
+        opacity: 1,
+        padding: 8,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5
+    },
+    textoBotao: {
+        color: "#fff",
+        alignItems: "center"
+    }
+});
