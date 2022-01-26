42cd2e3b059c5013e28e9e35873cf708cd3c291b
Zanini <everton.zanini@yahoo.com>
2022-01-26 12:56:38 -0300

    Criação do projeto no expo, implenetação do axios e da requisição http

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  FlatList
+ } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Filmes from './src/Filmes';
+
+import api from './src/services/api';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filmes:[]
+    }
+  }
+
+  async componentDidMount(){
+    const response = await api.get("r-api/?api=filmes");
+    this.setState({filmes: response.data});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.filmes}
+          keyExtractor={item=>item.id}
+          renderItem={({item})=><Filmes/>}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ src/Filmes/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default class Filmes extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render(){
+        return(
+            <Text>...</Text>
+        );
+    }
+}
